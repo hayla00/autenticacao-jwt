2d2b0acd59c506fda7cc26b9d8caade0d7dc087e
HAYLA ALZIRIO ALVES <haylaalves00@gmail.com>
2025-08-25 22:03:07 -0300

    Construção de  API 2.0

--- Src/App.ts
+++ Src/App.ts
@@ -2,8 +2,13 @@ import express from 'express';
 import { prismaClient, } from '../prisma/prisma.ts';
 import type { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-const app = express();
 
+enum userColumns {
+    NAME = "name",
+    EMAIL = "email",
+    PASSWORD = "password"
+}
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -14,7 +19,7 @@ app.get('/', (req, res) => {
 
 app.get('/users', async (req, res) => {
     try {
-        const users = await prismaClient.user.findMany(); 
+        const users = await prismaClient.user.findMany();
         res.json(users);
     } catch (error) {
         console.log(error);
@@ -27,37 +32,37 @@ app.get('/users/:id', async (req, res) => {
         const { params } = req;
 
         const user = await prismaClient.user.findUnique({
-            where:{
+            where: {
                 id: Number(params.id),
             }
-        }); 
-    
-        if(!user){
+        });
+
+        if (!user) {
             return res.status(404).json({
                 message: "Usuário não existe no banco de dados."
             })
         }
-    
-        return res.json(user); 
+
+        return res.json(user);
     } catch (error) {
         console.log(error);
         res.status(500).send(`Erro no servidor: ${error}`)
     }
 });
 
-app.post("/users", async(req, res)=>{
+app.post("/users", async (req, res) => {
     try {
         const { body } = req
         const userCreated = await prismaClient.user.create({
             data: {
                 name: body.name,
                 email: body.email,
-                password:body.password,
+                password: body.password,
             }
         })
         return res.status(201).json(userCreated)
     } catch (error) {
-        if((error as PrismaClientKnownRequestError).code == "P2002"){
+        if ((error as PrismaClientKnownRequestError).code == "P2002") {
             res.status(404).send('Email já cadastrado!')
         }
         console.log(error);
@@ -65,27 +70,51 @@ app.post("/users", async(req, res)=>{
     }
 })
 
-app.put("/users/:id", async(req, res)=>{
+app.put("/users/:id", async (req, res) => {
     try {
-        const {params, body} = req
+        const { params, body } = req
+        const bodyKeys:string[] = Object.keys(body)
+        for (const key of bodyKeys) {
+            if (key !== userColumns.NAME && 
+                key !== userColumns.PASSWORD && 
+                key !== userColumns.EMAIL) return res.status(404).send("Colunas não existentes")
+        }
         const user = await prismaClient.user.update({
             where: { id: Number(params.id) },
-            data: { 
-              ...body
-             },
-          })
+            data: {
+                ...body
+            },
+        })
         return res.status(200).json({
             message: "Usuário atualizado!",
             data: user
         })
     } catch (error) {
-        if((error as PrismaClientKnownRequestError).code == "P2025"){
+        if ((error as PrismaClientKnownRequestError).code == "P2025") {
             res.status(404).send('Usuário não encontrado!')
         }
         console.log(error);
         res.status(500).send(`Erro no servidor: ${error}`)
     }
-})  
+})
+
+app.delete("/users/:id", async(req, res)=>{
+    try {
+        const {params} = req
+        await prismaClient.user.delete({
+            where:{
+                id: Number(params.id)
+            }
+        })
+        res.status(200).send("Usuário deletado com sucesso!")
+    } catch (error) {
+        if ((error as PrismaClientKnownRequestError).code == "P2025") {
+            res.status(404).send('Usuário não encontrado!')
+        }
+        console.log(error);
+        res.status(500).send(`Erro no servidor: ${error}`)
+    }
+})
 
 
 app.listen(PORT, () => {
